d22e6cef4a43f870ec7a5b04f9b6732e1c25acda
Hugoagogo <Hughpowell2004@yahoo.com.au>
2011-04-04 22:16:23 +1000

    Lots more progress but kind of stuck on generating a perfect city, i think the problem lies within the edges at function

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import pyglet
 from pyglet import gl
 
 import random
+import copy
 
-import os
+import os,sys
+
+# open our log file
+so = se = open("test.log", 'w', 0)
+
+# re-open stdout without buffering
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+# redirect stdout and stderr to the log file opened above
+os.dup2(so.fileno(), sys.stdout.fileno())
 
 WINDOW_SIZE = (1024,768)
 TILE_SIZE = 128
@@ -32,13 +42,56 @@ def cycle_list(tlist,direction):
     return tlist
 
 def cycle_int(tint,direction,cap):
-    print tint
     tint = (direction+tint)%cap
     if tint < 1: tint = cap-tint
     return tint
 
 def build_2darray(x,y):
     return [[None]*x for y in range(y)]
+
+def build_perfect_grid(grid,tiles):
+    grid.pprint()
+    print
+    for row in range(len(grid.grid)):
+        for col in range(len(grid.grid[row])):
+            print col,row
+            if grid[row][col] == None:
+                sides = grid.edges_at(col,row)
+                print sides
+                possibles = []
+                
+                for tile in tiles:
+                    cmp = compare_sides(sides, tile.sides)
+                    if cmp != -1:
+                        print tile
+                        tile.rotate(cmp)
+                        print tile
+                        possibles.append(copy.deepcopy(tile))
+                random.shuffle(possibles)
+                print possibles
+                for possible in possibles:
+                    grid.place(possible,col,row)
+                    done, grid2 = build_perfect_grid(grid,tiles)
+                    if done:
+                        return True, grid2
+                return False, None
+    return True, grid
+        
+def compare_sides(sides1,sides2):
+    sides2 = sides2[:]
+    for x in range(len(sides1)):
+        if same_sides(sides1, sides2):
+            return x
+        sides2 = cycle_list(sides2,1)
+    else:
+        return -1
+
+def same_sides(sides1,sides2):
+    for s1, s2 in zip(sides1,sides2):
+        if not (s1 == s2 or s1 == "#" or s2 == "#"):
+            return False
+    return True
+        
 
 class TileLoadError(Exception):
     "This is raised when loading a tile fails for whatever reason"
@@ -90,20 +143,30 @@ class Grid(object):
         self.unused_tiles = []
         self.scale = scale
         
+    def tiles_to_fill(self):
+        print "STARTED"
+        flag, grid = build_perfect_grid(Grid(self.win,self.scale),self.win.all_tiles)
+        if flag:
+            self.grid = grid
+            print "DONE"
+        else:
+            print "DAMIT"
+        
     def drop(self,tile,x,y):
-        print tile, x
         if tile.x < (9*self.win.height*self.scale):
             x = self.win.screen2grid(x-(TILE_SIZE*self.scale)/2)
             y = self.win.screen2grid(y-(TILE_SIZE*self.scale)/2)
-            print x
-            tile.scale = self.scale
-            tile.x = (x+0.5)*TILE_SIZE*self.scale
-            print x*TILE_SIZE*self.scale
-            tile.y = (y+0.5)*TILE_SIZE*self.scale
-
-            self.grid[x][y] = tile
+            self.place(tile,x,y)
         else:
-            tile.scale
+            tile.scale = self.scale/2
+            tile.x = x
+            tile.y = y
+        
+    def place(self,tile,x,y):
+        tile.scale = self.scale
+        tile.x = (x+0.5)*TILE_SIZE*self.scale
+        tile.y = (y+0.5)*TILE_SIZE*self.scale
+        self.grid[y][x] = tile
         
     def draw(self):
         gl.glBegin(gl.GL_LINES)
@@ -127,6 +190,30 @@ class Grid(object):
         
         for tile in self.unused_tiles:
             tile.draw()
+    
+    def edges_at(self,x,y):
+        edges = []
+        for deltano, delta in enumerate([(0,-1),(-1,0),(0,1),(1,0)]):
+            px, py = x+delta[0],y+delta[1]
+            print px,py, delta,
+            if 0 <= px < len(self.grid[0]) and 0 <= py < len(self.grid):
+                print self.grid[py][px]
+                if self.grid[py][px]:
+                    edges.append(self.grid[py][px].sides[deltano])
+                else:
+                    edges.append("#")
+            else:
+                edges.append("g")
+                print "bang"
+        return edges
+    def __getitem__(self,key):
+        return self.grid[key]
+        
+    def pprint(self):
+        print "GRID OBJECT"
+        for line in reversed(self.grid): print line
+                
+        
 
 class GameWindow(pyglet.window.Window):
     def __init__(self,*args, **kwargs):
@@ -140,8 +227,8 @@ class GameWindow(pyglet.window.Window):
         for x in range(3):
             self.grid.drop(random.choice(self.all_tiles),int(x*(TILE_SIZE*self.grid.scale))+5,20)
         
-    def on_mouse_down(self):
-        pass
+    def on_mouse_press(self,x,y,button,modifiers):
+        self.grid.tiles_to_fill()
     
     def on_mouse_move(self):
         print screen2grid(x), screen2grid(y)
